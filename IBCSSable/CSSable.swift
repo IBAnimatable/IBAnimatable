f879bbd116692393b5b4e9c2b09f078f6044319a
Jake Lin <jakelinau@gmail.com>
2015-11-18 23:31:49 +1100

    Support more CSS properties

--- IBCSSable/CSSable.swift
+++ IBCSSable/CSSable.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 public protocol CSSable {
+  /**
+    `border-color`
+  */
   var borderColor: UIColor { get set }
+
+  /**
+    `border-width`
+  */
   var borderWidth: CGFloat { get set }
+
+  /**
+   `border-radius`
+  */
   var borderRadius: CGFloat { get set }
+
+  /**
+   `color` when using with `box-shadow`
+  */
+  var shadowColor: UIColor { get set }
+
+  /**
+    Radius in `box-shadow`
+  */
+  var shadowRadius: CGFloat { get set }
+
+  /**
+   Opacity in `box-shadow`: from 0 to 1
+  */
+  var shadowOpacity: CGFloat { get set }
+
+  /**
+   Horizontal offset in `box-shadow`
+  */
+  var shadowOffsetX: CGFloat { get set }
+
+  /**
+   Vertical offset in `box-shadow`
+  */
+  var shadowOffsetY: CGFloat { get set }
 }
 
-extension CSSable where Self:UIView {
+public extension CSSable where Self:UIView {
 //  // Only instance properties can be declared @IBInspectable
 //  // @IBInspectable 
 //  public var borderRadius: CGFloat {
@@ -38,5 +74,25 @@ extension CSSable where Self:UIView {
   public func configBorderRadius() {
     layer.cornerRadius = borderRadius
     layer.masksToBounds = borderRadius > 0
+  }
+  
+  public func configShadowColor() {
+    layer.shadowColor = shadowColor.CGColor
+  }
+
+  public func configShadowRadius() {
+    layer.shadowRadius = shadowRadius
+  }
+
+  public func configShadowOpacity() {
+    layer.shadowOpacity = Float(shadowOpacity)
+  }
+
+  public func configShadowOffsetX() {
+    layer.shadowOffset.width = shadowOffsetX
+  }
+
+  public func configShadowOffsetY() {
+    layer.shadowOffset.height = shadowOffsetY
   }
 }
