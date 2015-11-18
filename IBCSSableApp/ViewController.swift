01bb31f0640fe00672c11b42b52177933df4df29
Jake Lin <jakelinau@gmail.com>
2015-11-18 11:13:12 +1100

    Create the App

---
+++ IBCSSableApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBCSSableApp
+//
+//  Created by Jake Lin on 11/18/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
