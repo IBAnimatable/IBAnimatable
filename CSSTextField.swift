48f6bf8fa72f8f47cf1ffd3a7f0554d87bfac622
Jake Lin <jakelinau@gmail.com>
2015-11-19 11:29:51 +1100

    Add `CSSTextField`

--- CSSTextField.swift
+++ CSSTextField.swift
@@ -1,14 +1,14 @@
 //
-//  CSSView.swift
+//  CSSTextField.swift
 //  IBCSSableApp
 //
-//  Created by Jake Lin on 11/18/15.
+//  Created by Jake Lin on 11/19/15.
 //  Copyright © 2015 Jake Lin. All rights reserved.
 //
 
 import UIKit
 
-@IBDesignable public class CSSView: UIView, CSSBorderable, CSSBoxShadowable {
+@IBDesignable public class CSSTextField: UITextField, CSSBorderable, CSSBoxShadowable {
   @IBInspectable public var borderColor: UIColor = UIColor.clearColor() {
     didSet {
       configBorderColor()
@@ -20,7 +20,7 @@ import UIKit
       configBorderWidth()
     }
   }
-
+  
   @IBInspectable public var borderRadius: CGFloat = 0 {
     didSet {
       configBorderRadius()
@@ -32,28 +32,29 @@ import UIKit
       configShadowColor()
     }
   }
-
+  
   @IBInspectable public var shadowRadius: CGFloat = 0 {
     didSet {
       configShadowRadius()
     }
   }
-
+  
   @IBInspectable public var shadowOpacity: CGFloat = 0 {
     didSet {
       configShadowOpacity()
     }
   }
-
+  
   @IBInspectable public var shadowOffsetX: CGFloat = 0 {
     didSet {
       configShadowOffsetX()
     }
   }
-
+  
   @IBInspectable public var shadowOffsetY: CGFloat = 0 {
     didSet {
       configShadowOffsetY()
     }
   }
 }
+
