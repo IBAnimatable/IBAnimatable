00f74132aa962d8dbc7f5de6880238af77818d02
Jake Lin <jakelinau@gmail.com>
2015-11-19 15:16:07 +1100

    Add `CSSTextView`

---
+++ IBCSSable/CSSTextView.swift
@@ -0,0 +1,29 @@
+//
+//  CSSTextView.swift
+//  IBCSSableApp
+//
+//  Created by Jake Lin on 11/19/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class CSSTextView: UITextView, CSSBorderable {
+  @IBInspectable public var borderColor: UIColor = UIColor.clearColor() {
+    didSet {
+      configBorderColor()
+    }
+  }
+  
+  @IBInspectable public var borderWidth: CGFloat = 0 {
+    didSet {
+      configBorderWidth()
+    }
+  }
+  
+  @IBInspectable public var borderRadius: CGFloat = 0 {
+    didSet {
+      configBorderRadius()
+    }
+  }
+}
