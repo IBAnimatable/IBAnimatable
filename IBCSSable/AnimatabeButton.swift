3c81c6aee29f81cdda4f8ac0ba9ebbade35d97db
Jake Lin <jakelinau@gmail.com>
2015-11-20 10:17:54 +1100

    Rename to `AnimatabeButton`

--- IBCSSable/AnimatabeButton.swift
+++ IBCSSable/AnimatabeButton.swift
@@ -5,7 +5,7 @@
 
 import UIKit
 
-@IBDesignable public class CSSButton: UIButton, BorderDesignable, BoxShadowDesignable {
+@IBDesignable public class AnimatabeButton: UIButton, BorderDesignable, BoxShadowDesignable {
   
   // MARK: - CSSBorderable
   @IBInspectable public var borderColor: UIColor = UIColor.clearColor() {
