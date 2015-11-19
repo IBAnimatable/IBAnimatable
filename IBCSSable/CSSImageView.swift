8c18e746fa8f93aadac8e324f011911056ca5aba
Jake Lin <jakelinau@gmail.com>
2015-11-19 14:37:35 +1100

    Add `CSSImageView`

---
+++ IBCSSable/CSSImageView.swift
@@ -0,0 +1,59 @@
+//
+//  CSSImageView.swift
+//  IBCSSableApp
+//
+//  Created by Jake Lin on 11/19/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class CSSImageView: UIImageView, CSSBorderable, CSSBoxShadowable {
+  @IBInspectable public var borderColor: UIColor = UIColor.clearColor() {
+    didSet {
+      configBorderColor()
+    }
+  }
+  
+  @IBInspectable public var borderWidth: CGFloat = 0 {
+    didSet {
+      configBorderWidth()
+    }
+  }
+  
+  @IBInspectable public var borderRadius: CGFloat = 0 {
+    didSet {
+      configBorderRadius()
+    }
+  }
+  
+  @IBInspectable public var shadowColor: UIColor = UIColor.clearColor() {
+    didSet {
+      configShadowColor()
+    }
+  }
+  
+  @IBInspectable public var shadowRadius: CGFloat = 0 {
+    didSet {
+      configShadowRadius()
+    }
+  }
+  
+  @IBInspectable public var shadowOpacity: CGFloat = 0 {
+    didSet {
+      configShadowOpacity()
+    }
+  }
+  
+  @IBInspectable public var shadowOffsetX: CGFloat = 0 {
+    didSet {
+      configShadowOffsetX()
+    }
+  }
+  
+  @IBInspectable public var shadowOffsetY: CGFloat = 0 {
+    didSet {
+      configShadowOffsetY()
+    }
+  }
+}
