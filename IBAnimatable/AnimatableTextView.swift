146b4a39f1ca5325e913d6a27f19fcec2a898649
Jake Lin <jakelinau@gmail.com>
2015-12-01 23:09:15 +1100

    Make `AnimatableTextView` animatable

--- IBAnimatable/AnimatableTextView.swift
+++ IBAnimatable/AnimatableTextView.swift
@@ -5,7 +5,7 @@
 
 import UIKit
 
-@IBDesignable public class AnimatableTextView: UITextView, CornerDesignable, BorderDesignable {
+@IBDesignable public class AnimatableTextView: UITextView, CornerDesignable, BorderDesignable, Animatable {
   
   // MARK: - CornerDesignable
   @IBInspectable public var cornerRadius: CGFloat = 0 {
@@ -25,5 +25,29 @@ import UIKit
     didSet {
       configBorderWidth()
     }
+  }
+  
+  // MARK: - Animatable
+  @IBInspectable public var animationType: String = ""
+  @IBInspectable public var duration: Double = 0.7
+  @IBInspectable public var delay: Double = 0
+  @IBInspectable public var force: CGFloat = 1
+  @IBInspectable public var damping: CGFloat = 0.7
+  @IBInspectable public var velocity: CGFloat = 0.7
+  @IBInspectable public var repeatCount: Float = 1
+  
+  // MARK: - Life cycle
+  public override func prepareForInterfaceBuilder() {
+    
+  }
+  
+  public override func awakeFromNib() {
+    
+  }
+  
+  public override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    startAnimation()
   }
 }
