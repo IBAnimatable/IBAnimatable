b796c45ae037f645174a918d7eed1cea26ae0df1
Jake Lin <jakelinau@gmail.com>
2015-12-02 23:33:42 +1100

    Add `GradientStartPoint` enum

---
+++ IBAnimatable/GradientStartPoint.swift
@@ -0,0 +1,18 @@
+//
+//  Created by Jake Lin on 12/2/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import Foundation
+
+public enum GradientStartPoint {
+  case Top
+  case TopRight
+  case Right
+  case BottomRight
+  case Bottom
+  case BottomLeft
+  case Left
+  case TopLeft
+}
+ 
