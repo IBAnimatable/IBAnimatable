ea2a8fb114a5bb801d293b2efe99c606246d413c
Jake Lin <jakelinau@gmail.com>
2015-12-09 00:21:59 +1100

    Add `BorderSide`

---
+++ IBAnimatable/BorderSide.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Jake Lin on 12/9/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import Foundation
+
+public enum BorderSide: String {
+  case Top
+  case Right
+  case Bottom
+  case Left
+}
