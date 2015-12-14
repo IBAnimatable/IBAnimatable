30fb014833dd6719947f714967d7643dd0e02c19
Jake Lin <jakelinau@gmail.com>
2015-12-14 21:58:11 +1100

    Add `ViewControllerDesignable` and `hideNavigationBar`

---
+++ IBAnimatable/DesignableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Created by Jake Lin on 12/14/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class DesignableViewController: UIViewController, ViewControllerDesignable {
+  @IBInspectable public var hideNavigationBar: Bool = false
+  
+  // MARK: - Lifecylce
+  public override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    confingHideNavigationBar()
+  }
+}
---
+++ IBAnimatable/ViewControllerDesignable.swift
@@ -0,0 +1,18 @@
+
+//
+//  Created by Jake Lin on 12/14/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+public protocol ViewControllerDesignable {
+  var hideNavigationBar: Bool { get set }
+}
+
+
+public extension ViewControllerDesignable where Self: UIViewController {
+  public func confingHideNavigationBar() {
+    self.navigationController?.navigationBarHidden = hideNavigationBar
+  }
+}
