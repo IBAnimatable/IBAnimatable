a792a02b5336eb01a267e9c953e51caacee9e821
Jake Lin <jakelinau@gmail.com>
2015-12-14 18:04:11 +1100

    Update the circle mask logic

--- IBAnimatable/MaskDesignable.swift
+++ IBAnimatable/MaskDesignable.swift
@@ -24,7 +24,18 @@ public extension MaskDesignable where Self: UIView {
   // MARK: - Private
   private func maskCircle() {
     let circle = CAShapeLayer()
-    circle.path = UIBezierPath(roundedRect: bounds, cornerRadius: bounds.size.height).CGPath
+    var minHeight: CGFloat
+    var minBounds: CGRect
+    if (bounds.width != bounds.height) {
+      minHeight = bounds.width
+      minBounds = bounds
+    }
+    else {
+      minHeight = min(bounds.width, bounds.height)
+      minBounds = CGRectMake(0, 0, minHeight, minHeight)
+    }
+    
+    circle.path = UIBezierPath(roundedRect: minBounds, cornerRadius: minHeight).CGPath
     circle.fillColor = UIColor.blackColor().CGColor
     layer.mask = circle
   }
