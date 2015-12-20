8f748b0cca4aabe2acc8c0f506cff04a16df4106
Jake Lin <jakelinau@gmail.com>
2015-12-20 15:41:16 +1100

    Add `AnimatableLabel `

---
+++ IBAnimatable/AnimatableLabel.swift
@@ -0,0 +1,37 @@
+//
+//  Created by Jake Lin on 12/20/15.
+//  Copyright © 2015 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class AnimatableLabel: UILabel, Animatable {
+  
+  // MARK: - Animatable
+  @IBInspectable public var animationType: String = ""
+  @IBInspectable public var duration: Double = 0.7
+  @IBInspectable public var delay: Double = 0
+  @IBInspectable public var force: CGFloat = 1
+  @IBInspectable public var damping: CGFloat = 0.7
+  @IBInspectable public var velocity: CGFloat = 0.7
+  @IBInspectable public var repeatCount: Float = 1
+  
+  // MARK: - Lifecycle
+  public override func prepareForInterfaceBuilder() {
+    configDesignableProperties()
+  }
+  
+  public override func awakeFromNib() {
+    configDesignableProperties()
+  }
+  
+  public override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    startAnimation()
+  }
+  
+  // MARK: - Private
+  private func configDesignableProperties() {
+  }
+}
