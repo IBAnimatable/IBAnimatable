af24510f71a56a8e9a2e7f6d37ff396914abdced
Jake Lin <jakelinau@gmail.com>
2015-12-23 22:25:50 +1100

    Add first animatable example in Playground. 🎊

--- IBAnimatable.playground/Contents.swift
+++ IBAnimatable.playground/Contents.swift
@@ -1,8 +1,15 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCPlayground
 import IBAnimatable
+let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
 
-let view = AnimatableView(frame: CGRectMake(0, 0, 50, 50))
-view.fillColor = UIColor.purpleColor()
-view.flash()
+XCPlaygroundPage.currentPage.liveView = containerView
+
+
+let view = AnimatableView(frame: CGRectMake(100, 100, 50, 50))
+containerView.addSubview(view)
+view.fillColor = UIColor(red: 0xba, green: 0x77, blue: 0xff, alpha: 1)
+view.maskType = "Circle"
+view.slideInLeft()
