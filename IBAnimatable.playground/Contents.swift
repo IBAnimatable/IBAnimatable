c902813d38a75975dc88f035c207114bda94c5ff
Jake Lin <jakelinau@gmail.com>
2015-12-23 14:06:10 +1100

    Add Playground file

---
+++ IBAnimatable.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import IBAnimatable
+
+let view = AnimatableView(frame: CGRectMake(0, 0, 50, 50))
+view.fillColor = UIColor.purpleColor()
+view.flash()
