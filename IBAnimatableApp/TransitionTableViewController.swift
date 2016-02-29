7f37307a8a55ec92e9c54c186e188743a429d177
Jake Lin <jakelinau@gmail.com>
2016-03-01 10:34:15 +1100

    Add `TransitionTableViewController` to demonstrate transition animations.

--- IBAnimatableApp/TransitionTableViewController.swift
+++ IBAnimatableApp/TransitionTableViewController.swift
@@ -7,9 +7,36 @@ import UIKit
 
 class TransitionTableViewController: UITableViewController {
 
+  private var transitionAnimations = [TransitionAnimationType.Fade, .FadeIn, .FadeOut, .SystemCubeFromLeft]
+
+  // MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // TODO: Loop through `TransitionAnimationType` enum to generate the table dynamically.
+  }
+
   // MARK: - UITableViewDataSource
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("transitionCell")
-    return cell!
+    guard let cell = tableView.dequeueReusableCellWithIdentifier("transitionCell") else {
+      return UITableViewCell()
+    }
+    cell.textLabel?.text = transitionAnimations[indexPath.row].rawValue
+    return cell
   }
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return transitionAnimations.count
+  }
+  
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    super.prepareForSegue(segue, sender: sender)
+    
+    guard let toViewController = segue.destinationViewController as? AnimatableNavigationController, path = tableView.indexPathForSelectedRow else {
+      return
+    }
+  
+    toViewController.transitionAnimationType = String(transitionAnimations[path.row])
+  }
+  
 }
