049078f4cb2cbc0d0009ad49961623a4075e98a1
Jake Lin <jakelinau@gmail.com>
2016-03-09 22:36:14 +1100

    Sport fast swipe to pop

--- IBAnimatable/PanInteractiveAnimator.swift
+++ IBAnimatable/PanInteractiveAnimator.swift
@@ -34,34 +34,41 @@ public class PanInteractiveAnimator: UIPercentDrivenInteractiveTransition {
       return
     }
     let translation = gestureRecognizer.translationInView(superview)
-//    let velocity = gestureRecognizer.velocityInView(superview)
+    let velocity = gestureRecognizer.velocityInView(superview)
     
     var progress: CGFloat
     let distance: CGFloat
+    let speed: CGFloat
     switch interactiveGestureType {
     case .PanHorizontally:
       distance = superview.frame.width
       progress = abs(translation.x / distance)
+      speed = abs(velocity.x)
     case .PanFromLeft:
       distance = superview.frame.width
       progress = translation.x / distance
+      speed = velocity.x
     case .PanFromRight:
       distance = superview.frame.width
       progress = -(translation.x / distance)
+      speed = -velocity.x
     case .PanVertically:
       distance = superview.frame.height
       progress = abs(translation.y / distance)
+      speed = abs(velocity.y)
     case .PanFromTop:
       distance = superview.frame.height
       progress = translation.y / distance
+      speed = velocity.y
     case .PanFromBottom:
       distance = superview.frame.height
       progress = -translation.y / distance
+      speed = -velocity.y
     default:
       return
     }
     
-//    print(progress)
+//    print("progress: \(progress), speed: \(speed)")
     progress = min(max(progress, 0), 0.99)
 
     switch gestureRecognizer.state {
@@ -73,10 +80,11 @@ public class PanInteractiveAnimator: UIPercentDrivenInteractiveTransition {
       updateInteractiveTransition(progress)
     case .Cancelled, .Ended:
       interacting = false
-      if progress < 0.5 || gestureRecognizer.state == .Cancelled {
-        cancelInteractiveTransition()
-      } else {
+      // Finish the transition when pass the threathold 
+      if progress > 0.5 || speed > 1000 {
         finishInteractiveTransition()
+      } else {
+        cancelInteractiveTransition()
       }
     default:
       // Something happened. cancel the transition.
