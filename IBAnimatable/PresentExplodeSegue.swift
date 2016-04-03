920364e35e7911909563d84b7c6f17d9e50f0bfe
Tom Baranes <tom.baranes@gmail.com>
2016-04-03 21:40:29 +0200

    Explode segue

---
+++ IBAnimatable/PresentExplodeSegue.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Tom Baranes on 03/04/16.
+//  Copyright © 2016 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+public class PresentExplodeSegue: UIStoryboardSegue {
+  public override func perform() {
+    destinationViewController.transitioningDelegate = PresenterManager.sharedManager().retrievePresenter(.Explode, interactiveGestureType: .Default)
+    sourceViewController.presentViewController(destinationViewController, animated: true, completion: nil)
+  }
+}
---
+++ IBAnimatable/PresentExplodeWithDismissInteractionSegue.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  IBAnimatableApp
+//
+//  Created by Tom Baranes on 03/04/16.
+//  Copyright © 2016 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+public class PresentExplodeWithDismissInteractionSegue: UIStoryboardSegue {
+  public override func perform() {
+    destinationViewController.transitioningDelegate = PresenterManager.sharedManager().retrievePresenter(.Explode, interactiveGestureType: .Default)
+    sourceViewController.presentViewController(destinationViewController, animated: true, completion: nil)
+  }
+}
