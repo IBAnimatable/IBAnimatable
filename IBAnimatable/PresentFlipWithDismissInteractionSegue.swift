7ef69a303b34fafefd0a3dd783bf75fa161c595e
Tom Baranes <tom.baranes@gmail.com>
2016-05-05 18:43:12 +0200

    Flip segues

---
+++ IBAnimatable/PresentFlipWithDismissInteractionSegue.swift
@@ -0,0 +1,16 @@
+
+//
+//  Created by Tom Baranes on 05/05/16.
+//  Copyright © 2016 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+public class PresentFlipWithDismissInteractionSegue: UIStoryboardSegue {
+  public override func perform() {
+    destinationViewController.transitioningDelegate = PresenterManager.sharedManager().retrievePresenter(.Flip(direction: .Left), interactiveGestureType: .Default)
+    sourceViewController.presentViewController(destinationViewController, animated: true, completion: nil)
+  }
+}
+
+
