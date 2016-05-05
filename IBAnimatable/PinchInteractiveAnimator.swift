6f59b30e98ec76b0180494cc6362ebde2a22dc4c
Jake Lin <jakelinau@gmail.com>
2016-05-05 22:21:35 +1000

    Support Pinch Open

--- IBAnimatable/PinchInteractiveAnimator.swift
+++ IBAnimatable/PinchInteractiveAnimator.swift
@@ -26,7 +26,7 @@ public class PinchInteractiveAnimator: InteractiveAnimator {
       case .Open:
         return gestureRecognizer.velocity > 0
       default:
-        return false
+        return true
       }
     default:
       return false
@@ -43,20 +43,20 @@ public class PinchInteractiveAnimator: InteractiveAnimator {
       startScale = gestureRecognizer.scale
     }
     
+    print("startScale: \(startScale) - scale: \(gestureRecognizer.scale)")
+    
     var progress: CGFloat
     let distance: CGFloat
     switch interactiveGestureType {
     case let .Pinch(direction):
       switch direction {
-        // TODO:
-      case .In:
+      case .Close:
         progress = 1.0 - gestureRecognizer.scale / startScale
-        print(progress)
-//      case .Out:
+      case .Open:
+        progress = gestureRecognizer.scale / startScale - 1.0
       default:
-        // TODO:
-        // For both Pinch In and Pinch Out
-        return (0, false)
+        // For both `.Close` and `.Open`
+        progress = abs(1.0 - gestureRecognizer.scale / startScale)
     }
     default:
       return (0, false)
