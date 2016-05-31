6a7291d622001374ad18a933d13e336b1f03199c
Mark Hamilton <TheDarkCode@users.noreply.github.com>
2016-05-30 22:23:48 -0700

    Update PresentFadeSegue.swift

--- IBAnimatable/PresentFadeSegue.swift
+++ IBAnimatable/PresentFadeSegue.swift
@@ -1,5 +1,6 @@
 //
 //  Created by Jake Lin on 2/28/16.
+//  Updated by Mark Hamilton on 5/30/16
 //  Copyright © 2016 Jake Lin. All rights reserved.
 //
 
@@ -7,7 +8,11 @@ import UIKit
 
 public class PresentFadeSegue: UIStoryboardSegue {
   public override func perform() {
-    destinationViewController.transitioningDelegate = PresenterManager.sharedManager().retrievePresenter(.Fade(direction: .Cross))
+      destinationViewController.transitioningDelegate = PresenterManager.sharedManager().retrievePresenter(.Fade)
     sourceViewController.presentViewController(destinationViewController, animated: true, completion: nil)
+  }
+  public func perform(completion: OptionalAnimatableCompletion) {
+    destinationViewController.transitioningDelegate = PresenterManager.sharedManager().retrievePresenter(.Fade)
+    sourceViewController.presentViewController(destinationViewController, animated: true, completion: completion)
   }
 }
