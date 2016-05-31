97c79b1a0bac04b97f5ba6edbd417d18ee09379b
Mark Hamilton <TheDarkCode@users.noreply.github.com>
2016-05-30 22:23:22 -0700

    Update DismissSegue.swift

--- IBAnimatable/DismissSegue.swift
+++ IBAnimatable/DismissSegue.swift
@@ -1,5 +1,6 @@
 //
 //  Created by Jake Lin on 12/14/15.
+//  Updated by Mark Hamilton on 5/30/16
 //  Copyright © 2015 Jake Lin. All rights reserved.
 //
 
@@ -8,5 +9,8 @@ import UIKit
 public class DismissSegue: UIStoryboardSegue {
   public override func perform() {
     sourceViewController.dismissViewControllerAnimated(true, completion: nil)
+  }
+  public func perform(completion: OptionalAnimatableCompletion) {
+    sourceViewController.dismissViewControllerAnimated(true, completion: completion)
   }
 }
