21d4ab8fcdcb0b7e5eed69fb7e02d169504d93ee
Mark Hamilton <TheDarkCode@users.noreply.github.com>
2016-06-07 02:40:43 -0700

    Merge

--- IBAnimatable/PresentPortalSegue.swift
+++ IBAnimatable/PresentPortalSegue.swift
@@ -1,6 +1,5 @@
 //
 //  Created by Tom Baranes on 17/04/16.
-//  Updated by Mark Hamilton on 5/30/16
 //  Copyright © 2016 Jake Lin. All rights reserved.
 //
 
@@ -10,9 +9,5 @@ public class PresentPortalSegue: UIStoryboardSegue {
   public override func perform() {
     destinationViewController.transitioningDelegate = PresenterManager.sharedManager().retrievePresenter(.Portal(direction: .Forward, params: []))
     sourceViewController.presentViewController(destinationViewController, animated: true, completion: nil)
-  }
-  public func perform(completion: OptionalAnimatableCompletion) {
-    destinationViewController.transitioningDelegate = PresenterManager.sharedManager().retrievePresenter(.Portal(direction: .Forward, params: []))
-    sourceViewController.presentViewController(destinationViewController, animated: true, completion: completion)
   }
 }
