e51fbfa303e733f558f3db84aacf01381b2d9c8a
Mark Hamilton <TheDarkCode@users.noreply.github.com>
2016-06-29 05:19:01 -0700

    Add AnimatableSlider.swift

--- IBAnimatable/AnimatableSlider.swift
+++ IBAnimatable/AnimatableSlider.swift
@@ -6,6 +6,7 @@
 import UIKit
 
 @IBDesignable public class AnimatableSlider: UIView, BorderDesignable, RotationDesignable, ShadowDesignable, Animatable {
+<<<<<<< ed8d736c953db7552bbe73449d05f1da9c11ab54
 
   // MARK: - BorderDesignable
   @IBInspectable public var borderColor: UIColor? {
@@ -95,4 +96,95 @@ import UIKit
   private func configAfterLayoutSubviews() {
     configBorder()
   }
+=======
+    
+    // MARK: - BorderDesignable
+    @IBInspectable public var borderColor: UIColor? {
+        didSet {
+            configBorder()
+        }
+    }
+    
+    @IBInspectable public var borderWidth: CGFloat = CGFloat.nan {
+        didSet {
+            configBorder()
+        }
+    }
+    
+    @IBInspectable public var borderSide: String? {
+        didSet {
+            configBorder()
+        }
+    }
+    
+    // MARK: - RotationDesignable
+    @IBInspectable public var rotate: CGFloat = CGFloat.nan {
+        didSet {
+            configRotate()
+        }
+    }
+    
+    // MARK: - ShadowDesignable
+    @IBInspectable public var shadowColor: UIColor? {
+        didSet {
+            configShadowColor()
+        }
+    }
+    
+    @IBInspectable public var shadowRadius: CGFloat = CGFloat.nan {
+        didSet {
+            configShadowRadius()
+        }
+    }
+    
+    @IBInspectable public var shadowOpacity: CGFloat = CGFloat.nan {
+        didSet {
+            configShadowOpacity()
+        }
+    }
+    
+    @IBInspectable public var shadowOffset: CGPoint = CGPoint(x: CGFloat.nan, y: CGFloat.nan) {
+        didSet {
+            configShadowOffset()
+        }
+    }
+    
+    // MARK: - Animatable
+    @IBInspectable public var animationType: String?
+    @IBInspectable public var autoRun: Bool = true
+    @IBInspectable public var duration: Double = Double.nan
+    @IBInspectable public var delay: Double = Double.nan
+    @IBInspectable public var damping: CGFloat = CGFloat.nan
+    @IBInspectable public var velocity: CGFloat = CGFloat.nan
+    @IBInspectable public var force: CGFloat = CGFloat.nan
+    @IBInspectable public var repeatCount: Float = Float.nan
+    @IBInspectable public var x: CGFloat = CGFloat.nan
+    @IBInspectable public var y: CGFloat = CGFloat.nan
+    
+    // MARK: - Lifecycle
+    public override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        configInspectableProperties()
+    }
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        configInspectableProperties()
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        configAfterLayoutSubviews()
+        autoRunAnimation()
+    }
+    
+    // MARK: - Private
+    private func configInspectableProperties() {
+        configAnimatableProperties()
+    }
+    
+    private func configAfterLayoutSubviews() {
+        configBorder()
+    }
+>>>>>>> Add AnimatableSlider.swift
 }
