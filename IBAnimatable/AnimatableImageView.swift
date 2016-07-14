6a981bc9bc63fcb502c48baad25df485fa569e23
Tom Baranes <tom.baranes@gmail.com>
2016-07-14 15:16:17 +0200

    Fix conflict for cherry-pick.

--- IBAnimatable/AnimatableImageView.swift
+++ IBAnimatable/AnimatableImageView.swift
@@ -91,6 +91,12 @@ import UIKit
     }
   }
 
+  @IBInspectable public var vibrancyEffectStyle: String? {
+    didSet {
+      configBlurEffectStyle()
+    }
+  }
+
   @IBInspectable public var blurOpacity: CGFloat = CGFloat.NaN {
     didSet {
       configBlurEffectStyle()
