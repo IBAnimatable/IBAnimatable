f176e43414e89ed1c52dd113eaa63f0525c37b58
Jake Lin <jakelinau@gmail.com>
2016-07-31 11:42:56 +1000

    Move documentation comments up to `PresentationDesignable`

--- IBAnimatable/AnimatableModalViewController.swift
+++ IBAnimatable/AnimatableModalViewController.swift
@@ -10,112 +10,96 @@ public class AnimatableModalViewController: UIViewController, PresentationDesign
 
   // MARK: - AnimatablePresentationController
 
-  /// Presentation animation type, all supported animation type can be found in `PresentationAnimationType`
   @IBInspectable public var presentationAnimationType: String? {
     didSet {
       setupPresenter()
     }
   }
 
-  /// Dismissal animation type, all supported animation type can be found in `PresentationAnimationType`
   @IBInspectable public var dismissalAnimationType: String? {
     didSet {
       setupPresenter()
     }
   }
 
-  /// Tranistion duration
   @IBInspectable public var transitionDuration: Double = .NaN {
     didSet {
       presenter?.transitionDuration = transitionDuration
     }
   }
 
-  // Modal position, all supported modal position can be found in `PresentationModalPosition`
   @IBInspectable public var modalPosition: String = "Center" {
     didSet {
       presenter?.presentationConfiguration?.modalPosition = PresentationModalPosition.fromString(modalPosition)
     }
   }
 
-  // Modal width, all supported modal width can be found in `PresentationModalSize`
   @IBInspectable public var modalWidth: String = "Half" {
     didSet {
       presenter?.presentationConfiguration?.modalSize = (PresentationModalSize.fromString(modalWidth), PresentationModalSize.fromString(modalHeight))
     }
   }
 
-  // Modal height, all supported modal width can be found in `PresentationModalSize`
   @IBInspectable public var modalHeight: String = "Half" {
     didSet {
       presenter?.presentationConfiguration?.modalSize = (PresentationModalSize.fromString(modalWidth), PresentationModalSize.fromString(modalHeight))
     }
   }
 
-  /// The corner radius of the modal view
   @IBInspectable public var cornerRadius: CGFloat = .NaN {
     didSet {
       presenter?.presentationConfiguration?.cornerRadius = cornerRadius
     }
   }
 
-  /// If set to `true`, the modal view will dismiss when tap on the dimming view.
   @IBInspectable public var dismissOnTap: Bool = true {
     didSet {
       presenter?.presentationConfiguration?.dismissOnTap = dismissOnTap
     }
   }
 
-  /// The background color of the dimming view. The default value is black color.
   @IBInspectable public var backgroundColor: UIColor = .blackColor() {
     didSet {
       presenter?.presentationConfiguration?.backgroundColor = backgroundColor
     }
   }
 
-  // The opacity of the dimming view. The default value is `0.7`.
   @IBInspectable public var opacity: CGFloat = 0.7 {
     didSet {
       presenter?.presentationConfiguration?.opacity = opacity
     }
   }
 
-  // The blur effect style of the dimming view. If use this property, `backgroundColor` and `opacity` are ignored.
   @IBInspectable public var blurEffectStyle: String? {
     didSet {
       presenter?.presentationConfiguration?.blurEffectStyle = blurEffectStyle
     }
   }
 
-  // The blur opacity of the dimming view. If use this property, `backgroundColor` and `opacity` are ignored.
   @IBInspectable public var blurOpacity: CGFloat = .NaN {
     didSet {
       presenter?.presentationConfiguration?.blurOpacity = blurOpacity
     }
   }
 
-  // The shadow color of the modal view. If use this property, `cornerRadius` is ignored.
   @IBInspectable public var shadowColor: UIColor? {
     didSet {
       presenter?.presentationConfiguration?.shadowColor = shadowColor
     }
   }
 
-  // The shadow radius of the modal view. If use this property, `cornerRadius` is ignored.
   @IBInspectable public var shadowRadius: CGFloat = CGFloat.NaN {
     didSet {
       presenter?.presentationConfiguration?.shadowRadius = shadowRadius
     }
   }
 
-  // The shadow opacity of the modal view. If use this property, `cornerRadius` is ignored.
   @IBInspectable public var shadowOpacity: CGFloat = CGFloat.NaN {
     didSet {
       presenter?.presentationConfiguration?.shadowOpacity = shadowOpacity
     }
   }
 
-  // The shadow offset of the modal view. If use this property, `cornerRadius` is ignored.
   @IBInspectable public var shadowOffset: CGPoint = CGPoint(x: CGFloat.NaN, y: CGFloat.NaN) {
     didSet {
       presenter?.presentationConfiguration?.shadowOffset = shadowOffset
