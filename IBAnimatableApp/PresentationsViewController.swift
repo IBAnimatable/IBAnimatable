846e50c5332b2cf27cf6300a745da2a30b382db5
Jake Lin <jakelinau@gmail.com>
2016-08-02 09:13:09 +1000

    Dirty hack to make `Flip` and `CrossDissolve` work properly for dismissal transition in the demo app.

--- IBAnimatableApp/PresentationsViewController.swift
+++ IBAnimatableApp/PresentationsViewController.swift
@@ -19,13 +19,25 @@ class PresentationsViewController: AnimatableViewController {
     super.prepareForSegue(segue, sender: sender)
     if let toViewController = segue.destinationViewController as? AnimatableModalViewController {
       toViewController.presentationAnimationType = presentationAnimationType
+      setDismissalAnimationTypeIfNeeded(toViewController)
     }
   }
 
   @IBAction func presentProgramatically() {
-    if let presentedViewController = UIStoryboard(name: "Presentations", bundle: nil).instantiateViewControllerWithIdentifier("PresentationPresentedViewController") as? PresentationPresentedViewController {
+    if let presentedViewController = UIStoryboard(name: "Presentations", bundle: nil).instantiateViewControllerWithIdentifier("PresentationPresentedViewController") as? AnimatableModalViewController {
       presentedViewController.presentationAnimationType = presentationAnimationType
+      setDismissalAnimationTypeIfNeeded(presentedViewController)
       presentViewController(presentedViewController, animated: true, completion: nil)
+    }
+  }
+  
+  private func setDismissalAnimationTypeIfNeeded(viewController: AnimatableModalViewController) {
+    // FIXME: Dirty hack to make `Flip` and `CrossDissolve` work properly for dismissal transition.
+    // If we don't apply this hack, both the dismissal transitions of `Flip` and `CrossDissolve` will slide down the modal not flip or crossDissolve(fade).
+    if viewController.presentationAnimationType == "Flip" {
+      viewController.dismissalAnimationType = "Flip"
+    } else if viewController.presentationAnimationType == "CrossDissolve" {
+      viewController.dismissalAnimationType = "CrossDissolve"
     }
   }
 }
