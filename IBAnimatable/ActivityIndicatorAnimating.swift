643362f4f3b07ac762ac8194776a0ea431c8316e
Jake Lin <jakelinau@gmail.com>
2016-09-04 22:46:15 +1000

    Add documentation comments for `ActivityIndicatorAnimating`

--- IBAnimatable/ActivityIndicatorAnimating.swift
+++ IBAnimatable/ActivityIndicatorAnimating.swift
@@ -5,6 +5,14 @@
 
 import UIKit
 
+/// Protocol for all activity indicator classes.
 public protocol ActivityIndicatorAnimating {
+  /**
+   Define the animation for the activity indicator.
+   
+   - Parameter layer: The layer to execute the animation
+   - Parameter size: The size of the activity indicator.
+   - Parameter color: The color of the activity indicator.
+   */
   func configAnimation(in layer: CALayer, size: CGSize, color: UIColor)
 }
