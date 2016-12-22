3091f65b2c4460658a159b036c95f3c5bc9cf5d8
Tom Baranes <tom.baranes@gmail.com>
2016-12-22 10:45:17 +0100

    Add custom gradient start point

--- IBAnimatable/GradientStartPoint.swift
+++ IBAnimatable/GradientStartPoint.swift
@@ -4,8 +4,9 @@
 //
 
 import Foundation
+import UIKit
 
-public enum GradientStartPoint: String, IBEnum {
+public enum GradientStartPoint: IBEnum {
   case top
   case topRight
   case right
@@ -14,4 +15,45 @@ public enum GradientStartPoint: String, IBEnum {
   case bottomLeft
   case left
   case topLeft
+  case custom(start: CGPoint, end: CGPoint)
+  case none
+}
+
+extension GradientStartPoint {
+
+  public init(string: String?) {
+    guard let string = string else {
+      self = .none
+      return
+    }
+
+    let nameAndParams = AnimationType.extractNameAndParams(from: string)
+    let name = nameAndParams.name
+    let params = nameAndParams.params
+    switch name {
+    case "top":
+      self = .top
+    case "topRight":
+      self = .topRight
+    case "right":
+      self = .right
+    case "bottomRight":
+      self = .bottomRight
+    case "bottom":
+      self = .bottom
+    case "bottomLeft":
+      self = .bottomLeft
+    case "left":
+      self = .left
+    case "topLeft":
+      self = .topLeft
+    case "custom":
+      self = .custom(start: CGPoint(x: params[safe: 0]?.toDouble() ?? 0,
+                                    y: params[safe: 1]?.toDouble() ?? 0),
+                     end: CGPoint(x: params[safe: 2]?.toDouble() ?? 0,
+                                  y: params[safe: 3]?.toDouble() ?? 0))
+    default:
+      self = .none
+    }
+  }
 }
