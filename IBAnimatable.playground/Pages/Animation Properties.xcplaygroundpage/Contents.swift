ead675a2b449f8441ab206b798d247939ebe7f15
Jake Lin <jakelinau@gmail.com>
2017-03-12 23:12:11 +1100

    Update `Animation Properties` playground to apply custom animation factors to `then` method.

--- IBAnimatable.playground/Pages/Animation Properties.xcplaygroundpage/Contents.swift
+++ IBAnimatable.playground/Pages/Animation Properties.xcplaygroundpage/Contents.swift
@@ -24,7 +24,7 @@ let duration: TimeInterval = 1
 let delay: TimeInterval = 1
 
 //: damping: used in UIView Spring animation (0 ~ 1). To smoothly decelerate the animation without oscillation, use a value of 1. Employ a damping ratio closer to zero to increase oscillation. Default is 0.7. Notice: FadeOutIn, FadeInOut, Shake, Pop, Morph, Squeeze, Flash, Wobble and Swing animations do not use damping.
-let damping: CGFloat = 0.5
+let damping: CGFloat = 0.8
 
 //: velocity: used in UIView Spring animation. A value of 1 corresponds to the total animation distance traversed in one second. For example, if the total animation distance is 200 points and you want the start of the animation to match a view velocity of 100 pt/s, use a value of 0.5. Default is 0.7. Notice: FadeOutIn, FadeInOut, Shake, Pop, Morph, Squeeze, Flash, Wobble and Swing animations do not use damping.
 let velocity: CGFloat = 2
@@ -34,6 +34,6 @@ let force: CGFloat = 1
 
 view.animate(animationType, duration: duration, damping: damping, velocity: velocity, force: force)
     .delay(delay)
-    .then(.shake(repeatCount: 1))
+    .then(.shake(repeatCount: 3), duration: duration, damping: damping, velocity: velocity, force: force)
 
 //: [Next](@next)
