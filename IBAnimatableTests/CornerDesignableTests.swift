04b3b421fdd0adbffd663ec6e5ba682a437a9964
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-04-25 22:06:49 -0500

    Clean up CornerDesignableTests

--- IBAnimatableTests/CornerDesignableTests.swift
+++ IBAnimatableTests/CornerDesignableTests.swift
@@ -11,19 +11,21 @@ import XCTest
 
 class CornerDesignableTests: XCTestCase {
 
+  // MARK: - testCornerRadius
+
   func testCornerRadius() {
-    helper_testCornerRadius_UIView(element: AnimatableView(), message: "AnimatableView failed")
-    helper_testCornerRadius_UIView(element: AnimatableCheckBox(), message: "AnimatableCheckBox failed")
-    helper_testCornerRadius_UICollectionViewCell(element: AnimatableCollectionViewCell(), message: "AnimatableCollectionViewCell failed")
-    helper_testCornerRadius_UIView(element: AnimatableImageView(), message: "AnimatableImageView failed")
-    helper_testCornerRadius_UIView(element: AnimatableLabel(), message: "AnimatableLabel failed")
-    helper_testCornerRadius_UIView(element: AnimatableStackView(), message: "AnimatableStackView failed")
-    helper_testCornerRadius_UIView(element: AnimatableTableViewCell(), message: "AnimatableTableViewCell failed")
-    helper_testCornerRadius_UIView(element: AnimatableStackView(), message: "AnimatableStackView failed")
-    helper_testCornerRadius_UIView(element: AnimatableView(), message: "AnimatableView failed")
+    helper_testCornerRadius_UIView(element: AnimatableView())
+    helper_testCornerRadius_UIView(element: AnimatableCheckBox())
+    helper_testCornerRadius_UICollectionViewCell(element: AnimatableCollectionViewCell())
+    helper_testCornerRadius_UIView(element: AnimatableImageView())
+    helper_testCornerRadius_UIView(element: AnimatableLabel())
+    helper_testCornerRadius_UIView(element: AnimatableStackView())
+    helper_testCornerRadius_UIView(element: AnimatableTableViewCell())
+    helper_testCornerRadius_UIView(element: AnimatableStackView())
+    helper_testCornerRadius_UIView(element: AnimatableView())
   }
 
-  private func helper_testCornerRadius_UIView(element: MockUIViewCornerDesignable, message: String) {
+  private func helper_testCornerRadius_UIView(element: MockUIViewCornerDesignable) {
     var element = element
     element.cornerRadius = 3.0
     element.cornerSides = .allSides
@@ -32,17 +34,14 @@ class CornerDesignableTests: XCTestCase {
     let mask = element.layer.mask as? CAShapeLayer
     XCTAssertEqual(mask?.frame, CGRect(origin: .zero, size: element.bounds.size))
     XCTAssertEqual(mask?.name, "cornerSideLayer")
-    let corners: UIRectCorner = [.bottomLeft, .bottomRight, .topLeft]
-    let cornerRadii = CGSize(width: element.cornerRadius, height: element.cornerRadius)
-    let mockPath = UIBezierPath(roundedRect: element.bounds, byRoundingCorners: corners, cornerRadii: cornerRadii).cgPath
+    let mockPath = cgPath(for: element, withCorners: [.bottomLeft, .bottomRight, .topLeft])
     XCTAssertEqual(mask?.path, mockPath)
   }
 
-  private func helper_testCornerRadius_UICollectionViewCell(element: MockUICollectionViewCellCornerDesignable, message: String) {
+  private func helper_testCornerRadius_UICollectionViewCell(element: MockUICollectionViewCellCornerDesignable) {
     var element = element
     element.cornerRadius = -1
     XCTAssertFalse(element.contentView.layer.masksToBounds)
-    element.layer.mask?.name = "cornerSideLayer"
     element.cornerRadius = 3.0
     XCTAssertNil(element.layer.mask)
     XCTAssertEqual(element.layer.cornerRadius, 0.0)
@@ -53,39 +52,44 @@ class CornerDesignableTests: XCTestCase {
     let mask = element.contentView.layer.mask as? CAShapeLayer
     XCTAssertEqual(mask?.frame, CGRect(origin: .zero, size: element.bounds.size))
     XCTAssertEqual(mask?.name, "cornerSideLayer")
-    let corners: UIRectCorner = [.bottomLeft, .bottomRight, .topLeft]
-    let cornerRadii = CGSize(width: element.cornerRadius, height: element.cornerRadius)
-    let mockPath = UIBezierPath(roundedRect: element.bounds, byRoundingCorners: corners, cornerRadii: cornerRadii).cgPath
+    let mockPath = cgPath(for: element, withCorners: [.bottomLeft, .bottomRight, .topLeft])
     XCTAssertEqual(mask?.path, mockPath)
     XCTAssertTrue(element.contentView.layer.masksToBounds)
   }
 
-  func test_cornerSides() {
-    helper_test_cornerSides(element: AnimatableButton(), message: "AnimatableButton failed")
-    helper_test_cornerSides(element: AnimatableCheckBox(), message: "AnimatableCheckBox failed")
-    helper_test_cornerSides(element: AnimatableCollectionViewCell(), message: "AnimatableCollectionViewCell failed")
-    helper_test_cornerSides(element: AnimatableImageView(), message: "AnimatableImageView failed")
-    helper_test_cornerSides(element: AnimatableLabel(), message: "AnimatableLabel failed")
-    helper_test_cornerSides(element: AnimatableStackView(), message: "AnimatableStackView failed")
-    helper_test_cornerSides(element: AnimatableTableViewCell(), message: "AnimatableTableViewCell failed")
-    helper_test_cornerSides(element: AnimatableTextField(), message: "AnimatableTextField failed")
-    helper_test_cornerSides(element: AnimatableTextView(), message: "AnimatableTextView failed")
-    helper_test_cornerSides(element: AnimatableView(), message: "AnimatableView failed")
+  private func cgPath(for element: MockUIViewCornerDesignable, withCorners corners: UIRectCorner) -> CGPath {
+    let cornerRadii = CGSize(width: element.cornerRadius, height: element.cornerRadius)
+    return UIBezierPath(roundedRect: element.bounds, byRoundingCorners: corners, cornerRadii: cornerRadii).cgPath
   }
 
-  private func helper_test_cornerSides(element: MockUIViewCornerDesignable, message: String) {
+  // MARK: test_cornerSides
+
+  func test_cornerSides() {
+    helper_test_cornerSides(element: AnimatableButton())
+    helper_test_cornerSides(element: AnimatableCheckBox())
+    helper_test_cornerSides(element: AnimatableCollectionViewCell())
+    helper_test_cornerSides(element: AnimatableImageView())
+    helper_test_cornerSides(element: AnimatableLabel())
+    helper_test_cornerSides(element: AnimatableStackView())
+    helper_test_cornerSides(element: AnimatableTableViewCell())
+    helper_test_cornerSides(element: AnimatableTextField())
+    helper_test_cornerSides(element: AnimatableTextView())
+    helper_test_cornerSides(element: AnimatableView())
+  }
+
+  private func helper_test_cornerSides(element: MockUIViewCornerDesignable) {
     element._cornerSides = "topLeft"
-    XCTAssertEqual(element.cornerSides, .topLeft, message)
+    XCTAssertEqual(element.cornerSides, .topLeft)
     element._cornerSides = "topRight"
-    XCTAssertEqual(element.cornerSides, .topRight, message)
+    XCTAssertEqual(element.cornerSides, .topRight)
     element._cornerSides = "bottomLeft"
-    XCTAssertEqual(element.cornerSides, .bottomLeft, message)
+    XCTAssertEqual(element.cornerSides, .bottomLeft)
     element._cornerSides = "bottomRight"
-    XCTAssertEqual(element.cornerSides, .bottomRight, message)
+    XCTAssertEqual(element.cornerSides, .bottomRight)
     element._cornerSides = "allSides"
-    XCTAssertEqual(element.cornerSides, .allSides, message)
+    XCTAssertEqual(element.cornerSides, .allSides)
     element._cornerSides = ""
-    XCTAssertEqual(element.cornerSides, .allSides, message)
+    XCTAssertEqual(element.cornerSides, .allSides)
   }
 
 }
