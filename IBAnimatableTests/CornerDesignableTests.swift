48a24f8e8a65f80262bf14e31fdb035ed42f6123
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-05-12 22:44:52 -0500

    Refactor to protocol oriented test approach

--- IBAnimatableTests/CornerDesignableTests.swift
+++ IBAnimatableTests/CornerDesignableTests.swift
@@ -6,4 +6,81 @@
 //  Copyright © 2017 IBAnimatable. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import IBAnimatable
+
+// MARK: - CornerDesignableTests Protocol
+
+protocol CornerDesignableTests: class {
+
+  func testCornerRadius()
+  func test_cornerSides()
+
+}
+
+// MARK: - Universal Tests
+
+extension CornerDesignableTests {
+
+  func _test_cornerSides(_ element: StringCornerDesignable) {
+    element._cornerSides = "topLeft"
+    XCTAssertEqual(element.cornerSides, .topLeft)
+    element._cornerSides = "topRight"
+    XCTAssertEqual(element.cornerSides, .topRight)
+    element._cornerSides = "bottomLeft"
+    XCTAssertEqual(element.cornerSides, .bottomLeft)
+    element._cornerSides = "bottomRight"
+    XCTAssertEqual(element.cornerSides, .bottomRight)
+    element._cornerSides = "allSides"
+    XCTAssertEqual(element.cornerSides, .allSides)
+    element._cornerSides = ""
+    XCTAssertEqual(element.cornerSides, .allSides)
+  }
+
+}
+
+// MARK: - UIView Tests
+
+extension CornerDesignableTests {
+
+  func _testCornerRadius<E: UIView>(_ element: E) where E: CornerDesignable {
+    element.cornerRadius = 3.0
+    element.cornerSides = .allSides
+    XCTAssertEqual(element.cornerRadius, element.layer.cornerRadius)
+    element.cornerSides = [.bottomLeft, .bottomRight, .topLeft]
+    let mask = element.layer.mask as? CAShapeLayer
+    XCTAssertEqual(mask?.frame, CGRect(origin: .zero, size: element.bounds.size))
+    XCTAssertEqual(mask?.name, "cornerSideLayer")
+    let cornerRadii = CGSize(width: element.cornerRadius, height: element.cornerRadius)
+    let corners: UIRectCorner = [.bottomLeft, .bottomRight, .topLeft]
+    let mockPath = UIBezierPath(roundedRect: element.bounds, byRoundingCorners: corners, cornerRadii: cornerRadii).cgPath
+    XCTAssertEqual(mask?.path, mockPath)
+  }
+
+}
+
+// MARK: - UICollectionViewCell Tests
+
+extension CornerDesignableTests {
+
+  func _testCornerRadius<E: UICollectionViewCell>(_ element: E) where E: CornerDesignable {
+    element.cornerRadius = -1
+    XCTAssertFalse(element.contentView.layer.masksToBounds)
+    element.cornerRadius = 3.0
+    XCTAssertNil(element.layer.mask)
+    XCTAssertEqual(element.layer.cornerRadius, 0.0)
+    element.cornerSides = .allSides
+    XCTAssertEqual(element.contentView.layer.cornerRadius, element.cornerRadius)
+    element.cornerSides = [.bottomLeft, .bottomRight, .topLeft]
+    XCTAssertEqual(element.contentView.layer.cornerRadius, 0.0)
+    let mask = element.contentView.layer.mask as? CAShapeLayer
+    XCTAssertEqual(mask?.frame, CGRect(origin: .zero, size: element.bounds.size))
+    XCTAssertEqual(mask?.name, "cornerSideLayer")
+    let cornerRadii = CGSize(width: element.cornerRadius, height: element.cornerRadius)
+    let corners: UIRectCorner = [.bottomLeft, .bottomRight, .topLeft]
+    let mockPath = UIBezierPath(roundedRect: element.bounds, byRoundingCorners: corners, cornerRadii: cornerRadii).cgPath
+    XCTAssertEqual(mask?.path, mockPath)
+    XCTAssertTrue(element.contentView.layer.masksToBounds)
+  }
+
+}
--- IBAnimatableTests/FillDesignableTests.swift
+++ IBAnimatableTests/FillDesignableTests.swift
@@ -6,4 +6,115 @@
 //  Copyright © 2017 IBAnimatable. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import IBAnimatable
+
+protocol FillDesignableTests {
+
+  func testFillColor()
+  func testOpacity()
+  func testPredefinedColor()
+  func test_predefinedColor()
+
+}
+
+// MARK: - Universal Tests
+
+extension FillDesignableTests {
+
+  func _test_predefinedColor(_ element: StringFillDesignable) {
+    element._predefinedColor = "flatEmerland"
+    XCTAssertEqual(element.predefinedColor, .flatEmerland)
+    element._predefinedColor = "flatPomegranate"
+    XCTAssertEqual(element.predefinedColor, .flatPomegranate)
+    element._predefinedColor = "flatWetAsphalt"
+    XCTAssertEqual(element.predefinedColor, .flatWetAsphalt)
+    element._predefinedColor = "flatTurquoise"
+    XCTAssertEqual(element.predefinedColor, .flatTurquoise)
+    element._predefinedColor = "flatConcrete"
+    XCTAssertEqual(element.predefinedColor, .flatConcrete)
+    element._predefinedColor = "flatOrange"
+    XCTAssertEqual(element.predefinedColor, .flatOrange)
+    element._predefinedColor = "flatAsbestos"
+    XCTAssertEqual(element.predefinedColor, .flatAsbestos)
+    element._predefinedColor = "flatPeterRiver"
+    XCTAssertEqual(element.predefinedColor, .flatPeterRiver)
+    element._predefinedColor =  "flatSilver"
+    XCTAssertEqual(element.predefinedColor, .flatSilver)
+    element._predefinedColor = "flatSunFlower"
+    XCTAssertEqual(element.predefinedColor, .flatSunFlower)
+    element._predefinedColor =  "flatAmethyst"
+    XCTAssertEqual(element.predefinedColor, .flatAmethyst)
+    element._predefinedColor =  "flatAlizarin"
+    XCTAssertEqual(element.predefinedColor, .flatAlizarin)
+    element._predefinedColor =  "flatGreenSea"
+    XCTAssertEqual(element.predefinedColor, .flatGreenSea)
+    element._predefinedColor =  "flatBelizeHole"
+    XCTAssertEqual(element.predefinedColor, .flatBelizeHole)
+    element._predefinedColor =  "flatNephritis"
+    XCTAssertEqual(element.predefinedColor, .flatNephritis)
+    element._predefinedColor =  "flatMidnightBlue"
+    XCTAssertEqual(element.predefinedColor, .flatMidnightBlue)
+    element._predefinedColor =  "flatClouds"
+    XCTAssertEqual(element.predefinedColor, .flatClouds)
+    element._predefinedColor =  "flatWisteria"
+    XCTAssertEqual(element.predefinedColor, .flatWisteria)
+    element._predefinedColor =  "flatCarrot"
+    XCTAssertEqual(element.predefinedColor, .flatCarrot)
+    element._predefinedColor =  "flatPumpkin"
+    XCTAssertEqual(element.predefinedColor, .flatPumpkin)
+  }
+
+}
+
+// MARK: - UIView Tests
+
+extension FillDesignableTests {
+
+  func _testFillColor<E: UIView>(_ element: E) where E: FillDesignable {
+    element.fillColor = .red
+    XCTAssertEqual(element.backgroundColor, .red)
+  }
+
+  func _testOpacity<E: UIView>(_ element: E) where E: FillDesignable {
+    element.opacity = 0.5
+    XCTAssertEqual(element.opacity, element.alpha)
+    XCTAssertFalse(element.isOpaque)
+    element.opacity = 1.0
+    XCTAssertEqual(element.opacity, element.alpha)
+    XCTAssertTrue(element.isOpaque)
+  }
+
+  func _testPredefinedColor<E: UIView>(_ element: E) where E: FillDesignable {
+    element.predefinedColor = .flatGreenSea
+    XCTAssertEqual(element.backgroundColor, ColorType.flatGreenSea.color)
+  }
+
+}
+
+// MARK: - UICollectionViewCell Tests
+
+extension FillDesignableTests {
+
+  func _testFillColor<E: UICollectionViewCell>(_ element: E) where E: FillDesignable {
+    element.fillColor = .red
+    XCTAssertEqual(element.backgroundColor, .red)
+    XCTAssertEqual(element.contentView.backgroundColor, .red)
+  }
+
+  func _testOpacity<E: UICollectionViewCell>(_ element: E) where E: FillDesignable {
+    element.opacity = 0.5
+    XCTAssertEqual(element.opacity, element.alpha)
+    XCTAssertFalse(element.isOpaque)
+    element.opacity = 1.0
+    XCTAssertEqual(element.opacity, element.alpha)
+    XCTAssertTrue(element.isOpaque)
+  }
+
+  func _testPredefinedColor<E: UICollectionViewCell>(_ element: E) where E: FillDesignable {
+    element.predefinedColor = .flatClouds
+    XCTAssertEqual(element.backgroundColor, ColorType.flatClouds.color)
+    XCTAssertEqual(element.contentView.backgroundColor, ColorType.flatClouds.color)
+  }
+
+}
