c63d363ddb4876c024e5730f6ea827830abd3ab8
Tom Baranes <tom.baranes@gmail.com>
2017-05-22 10:32:15 +0200

    Add keyboard translation `stickOrMoveUp`

--- Sources/Enums/PresentationKeyboardTranslation.swift
+++ Sources/Enums/PresentationKeyboardTranslation.swift
@@ -9,14 +9,23 @@ public enum ModalKeyboardTranslation: String, IBEnum {
   case none
   case moveUp
   case aboveKeyboard
+  case stickOrMoveUp
 
   public func translationFrame(keyboardFrame: CGRect, presentedFrame: CGRect) -> CGRect {
     let keyboardMinY = UIScreen.main.bounds.height - keyboardFrame.height
     var frame = presentedFrame
     switch self {
-    case .moveUp: frame.origin.y -= keyboardFrame.height; break
-    case .aboveKeyboard: frame.origin.y = keyboardMinY - presentedFrame.height - 20
-    default: break
+    case .moveUp:
+        frame.origin.y -= keyboardFrame.height
+        break
+    case .aboveKeyboard:
+        frame.origin.y = keyboardMinY - presentedFrame.height - 20
+    case .stickOrMoveUp:
+      if frame.maxY > keyboardMinY {
+        frame.origin.y = keyboardMinY - presentedFrame.height - 20
+      }
+    default:
+        break
     }
     return frame
   }
