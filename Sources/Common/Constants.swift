f3bab4a39af5d189640742cd1989501e25cc5e8d
Jake Lin <jake.lin@rea-group.com>
2017-09-12 11:54:55 +1000

    Make the default constants to be `public` to avoid the Swift bug https://bugs.swift.org/browse/SR-5111

--- Sources/Common/Constants.swift
+++ Sources/Common/Constants.swift
@@ -7,5 +7,5 @@ import Foundation
 
 // Default transition duration.
 // The default UIKit transtion animation duration is 0.35, but too fast for most of custom transition animations.
-let defaultTransitionDuration: Duration = 0.75
-let defaultPresentationDuration: Duration = 0.4
+public let defaultTransitionDuration: Duration = 0.75
+public let defaultPresentationDuration: Duration = 0.4
