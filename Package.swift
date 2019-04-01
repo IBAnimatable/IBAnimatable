f60b95338d07ad8451f12c705ffa7c29c9bd296f
Cihat Gündüz <commits@cihatguenduez.de>
2019-04-01 13:46:38 +0200

    Update SwiftPM manifest file

--- Package.swift
+++ Package.swift
@@ -1,3 +1,16 @@
+// swift-tools-version:4.2
 import PackageDescription
 
-let package = Package(name: "IBAnimatable")
+let package = Package(
+    name: "IBAnimatable",
+    // platforms: [.iOS("9.0")],
+    products: [
+        .library(name: "IBAnimatable", targets: ["IBAnimatable"])
+    ],
+    targets: [
+        .target(
+            name: "IBAnimatable",
+            path: "Sources"
+        )
+    ]
+)
