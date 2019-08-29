bfc9de2875b4c2efd8e064ac7883c4b1fd932795
Eric Marchand <eric.marchand@4d.com>
2019-08-29 16:02:10 +0200

    Change segue presentation style only if `pageSheet` or `automatic`

--- Sources/Extensions/UIStoryboardSegueExtension.swift
+++ Sources/Extensions/UIStoryboardSegueExtension.swift
@@ -7,11 +7,16 @@ import UIKit
 
 extension UIStoryboardSegue {
   func present(animated flag: Bool = true) {
+    #if swift(>=5.1)
     if #available(iOS 13.0, *) {
       if destination.transitioningDelegate != nil {
-        destination.modalPresentationStyle = .fullScreen // .automatic do not work well with our transitioningDelegate
+        let presentationStyle = destination.modalPresentationStyle
+        if presentationStyle == .automatic || presentationStyle == .pageSheet {
+          destination.modalPresentationStyle = .fullScreen // .automatic & .pageSheet do not work well with our transitioningDelegate
+        }
       }
     }
+    #endif
     source.present(destination, animated: flag, completion: nil)
   }
 }
